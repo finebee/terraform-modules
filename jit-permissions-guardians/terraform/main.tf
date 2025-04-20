8e9fc91e799a650841f072be41b1d3adb000f290
mevratFinebee <78731116+mevratFinebee@users.noreply.github.com>
2025-04-20 17:56:25 +0300

    comments

--- jit-permissions-guardians/terraform/main.tf
+++ jit-permissions-guardians/terraform/main.tf
@@ -78,7 +78,7 @@ resource "null_resource" "runner_env_setup" {
 }
 
 resource "kubiya_webhook" "webhook" {
-  // Required
+  // Required fileds
   //mandatory fields
   //Please specify a unique name to identify this webhook
   name = "${var.teammate_name} JIT webhook"
