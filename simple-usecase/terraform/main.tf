7d0df9322299b85bcc0fc71d3215782790b6db01
mevratFinebee <78731116+mevratFinebee@users.noreply.github.com>
2025-05-06 12:46:08 +0300

    update

--- simple-usecase/terraform/main.tf
+++ simple-usecase/terraform/main.tf
@@ -14,9 +14,9 @@ provider "kubiya" {
 resource "kubiya_agent" "jit_guardian" {
   name          = var.teammate_name
   runner        = var.kubiya_runner
-  description   = "teammate description"
-  model         = "azure/gpt-4o"
-  instructions  = "teammate ai instructions"
+  description   = var.teammate_description
+  model         = var.teammate_model
+  instructions  = var.instructions
   integrations  = var.kubiya_integrations
   users         = []
   groups        = var.kubiya_groups_allowed_groups
