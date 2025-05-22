44173fb1742eae529184403067aea47e86761c0b
mevratFinebee <78731116+mevratFinebee@users.noreply.github.com>
2025-05-22 17:50:53 +0300

    updated instructions

--- simple-usecase/terraform/main.tf
+++ simple-usecase/terraform/main.tf
@@ -16,7 +16,7 @@ resource "kubiya_agent" "jit_guardian" {
   runner        = var.kubiya_runner
   description   = var.teammate_description
   model         = var.teammate_model
-  instructions  = "this is ai instructions"
+  instructions  = "this is ai instructions updated instructions"
   integrations  = var.kubiya_integrations
   users         = []
   groups        = var.kubiya_groups_allowed_groups
